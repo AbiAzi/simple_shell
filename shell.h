2fb4b733380ce570d68139832c01d164aa77faea
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-11-12 09:59:23 -0930

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,6 +1,8 @@
 #ifndef SHELL
 #define SHELL
 
+#define READLINE_ERROR "error while reading input\n"
+
 #include <stddef.h>
 #include <signal.h>
 #include <stdlib.h>
@@ -8,8 +10,57 @@
 #include <string.h>
 #include <stdio.h>
 
-#include "typedefs.h"
-#include "constants.h"
+/**
+ * enum _bool - enumeration to use true and false
+ * @false: 0
+ * @true: 1
+ */
+typedef enum _bool
+{
+    false = 0,
+    true = 1
+} Bool;
+
+typedef enum errors
+{
+    READLINE = 0
+} Errors;
+
+/**
+ * struct list_s - struct defining a linked list
+ * @dir: a directory path
+ * @next: a pointer to another struct list_s
+ */
+typedef struct list_s
+{
+    char *dir;
+    struct list_s *next;
+} list_t;
+
+/**
+ * struct builtin_s - defines builtin commands
+ * @name: the builtin command's name
+ * @f: function pointer to the builtin command
+ */
+typedef struct builtin_s
+{
+    char *name;
+    int (*f)(char **argv, char **front);
+} builtin_t;
+
+/**
+ * struct alias_s - defines aliases
+ * @name: alias' name
+ * @value: alias' value
+ * @next: a pointer to the next alias
+ */
+typedef struct alias_s
+{
+    char *name;
+    char *value;
+    struct alias_s *next;
+} alias_t;
+
 
 extern char **environ;
 char *name;
