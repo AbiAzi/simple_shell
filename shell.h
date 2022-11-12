111c9a858f9258f392c9f9995829a283ebacade7
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-11-12 09:55:50 -0930

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,11 +1,26 @@
 #ifndef SHELL
 #define SHELL
 
-#include <stdio.h>
+#include <stddef.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <stdio.h>
+
+#include "typedefs.h"
+#include "constants.h"
+
+extern char **environ;
+char *name;
+alias_t *aliases;
+void signal_handler(int);
+char *get_user_input();
+void readline_error(void);
+
+void (*handle_error[])(void) = {
+    readline_error
+};
+
 
 #endif
